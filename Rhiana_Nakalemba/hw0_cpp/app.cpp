927d47f9cdd283d7365e1fb6abfad2dc8ce686a4
trhiana <trhiana@rocketmail.com>
2022-01-21 19:14:35 +0000

    Added hw0_cpp folder

---
+++ Rhiana_Nakalemba/hw0_cpp/app.cpp
@@ -0,0 +1,12 @@
+// Simple program after installing C++
+// Using CLION IDE
+// Written by Rhiana Thelemaque
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, I am all set up for C++ for ICP";
+
+	return 0;
+}
