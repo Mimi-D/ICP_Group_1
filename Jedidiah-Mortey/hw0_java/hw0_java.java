de405c450a9c8a3b1876ce0d2e6aece0d90b2e1f
Jedi-305 <98164548+Jedi-305@users.noreply.github.com>
2022-01-21 17:57:24 +0000

    Homework 0 Java

---
+++ Jedidiah-Mortey/hw0_java/hw0_java.java
@@ -0,0 +1,10 @@
+package hw0;
+
+class Hw0_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, I am all set for Java for ICP.");
+	}
+
+}
